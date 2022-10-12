f9bd078ef59db7e02fa757ea93aad4c8d432aa4d
speddings <106454096+speddings@users.noreply.github.com>
2022-10-12 14:49:08 -0500

    update r-script file

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -15,3 +15,39 @@ lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AW
 # Using the summary() function, determine the p-value and the r-squared value for the linear regression model.
 summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data=mecha_mpg)) 
 
+
+# DELIVERABLE 2
+# import and read in the Suspension_Coil.csv file as a table
+suspension_mpg <- read.csv(file='./resources/Suspension_Coil.csv',check.names=F,stringsAsFactors = F) 
+head(suspension_mpg)
+
+# Create a total_summary dataframe using the summarize() function to get the mean, median, variance, 
+# and standard deviation of the suspension coil’s PSI column.
+total_summary <- suspension_mpg %>% summarize(Mean=mean(PSI),Median=(PSI),Variance=var(PSI),SD=sd(PSI))
+
+# creates a lot_summary dataframe using the group_by() and the summarize() functions to 
+# group each manufacturing lot by the mean, median, variance, and standard deviation of 
+# the suspension coil’s PSI column.
+lot_summary <- suspension_mpg %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI),Median = median(PSI),Variance = var(PSI),SD = sd(PSI), .groups = 'keep') 
+
+
+
+# DELIVERABLE 3
+# determine if the PSI across all manufacturing lots is statistically different 
+# from the population mean of 1,500 pounds per square inch.
+?t.test()
+t.test(suspension_mpg$PSI,mu=1500)
+
+# 3 more scripts using t.test() function and its subset() argument to determine if the PSI 
+# for each Lot is statistically different from the population mean of 1,500 psi.
+
+# LOT 1
+t.test(subset(suspension_mpg,Manufacturing_Lot=="Lot1")$PSI,mu=1500)
+
+# LOT 2
+t.test(subset(suspension_mpg,Manufacturing_Lot=="Lot2")$PSI,mu=1500)
+
+# LOT 3
+t.test(subset(suspension_mpg,Manufacturing_Lot=="Lot3")$PSI,mu=1500)
+              
+              
